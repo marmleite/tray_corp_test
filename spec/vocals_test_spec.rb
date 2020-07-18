91c0c87e540e891ffd73204bb22db8066cc520bc
marcelo <marmleite@hotmail.com>
2020-07-18 17:19:54 -0300

    test argument not String VocalsTestSpec

--- spec/vocals_test_spec.rb
+++ spec/vocals_test_spec.rb
@@ -1,2 +1,9 @@
+require './../problems/vocals_test'
+
 RSpec.describe VocalsTest do
+  context "argument is not a String" do
+    it "returns invalid argument" do
+      expect{VocalsTest.sum(nil)}.to raise_error('invalid argument')
+    end
+  end
 end
