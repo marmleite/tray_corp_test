0dd9e5b32f3c95166632a8404c537e4253100327
marcelo <marmleite@hotmail.com>
2020-07-18 18:17:52 -0300

    count letter to vocal_hash VocalsTest

--- problems/vocals_test.rb
+++ problems/vocals_test.rb
@@ -3,5 +3,16 @@ class VocalsTest
   VOCALS_HASH = {
     a: 0, e: 0, i: 0, o: 0, u: 0
   }.freeze
+  
+  class << self
+    def add_count_vocal(hash, letter)
+      unless (letter =~ /[aeiou]/) == nil
+        hash[letter.to_sym] += 1
+      end
+    end
 
+    def vocals_hash
+      VOCALS_HASH.dup
+    end
+  end
 end
