862ef24c195906dfa651359b89152baaf26e45bd
marcelo <marmleite@hotmail.com>
2020-07-18 18:15:21 -0300

    test count letter to hash

--- spec/vocals_test_spec.rb
+++ spec/vocals_test_spec.rb
@@ -1,9 +1,17 @@
 require './../problems/vocals_test'
 
 RSpec.describe VocalsTest do
-  context "argument is not a String" do
-    it "returns invalid argument" do
+  context 'argument is not a String' do
+    it 'returns invalid argument' do
       expect{VocalsTest.sum(nil)}.to raise_error('invalid argument')
+    end
+  end
+
+  context 'pass a letter with hash to count' do
+    it 'returns vocals hash with count ' do
+      vocals_hash = VocalsTest.vocals_hash
+      VocalsTest.add_count_vocal(vocals_hash, 'a') 
+      expect(vocals_hash).to eq({ a: 1, e: 0, i: 0, o: 0, u: 0 })
     end
   end
 end
