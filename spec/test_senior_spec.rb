f36a26525e47e448097fc26eb83f67f3652125d9
marcelo <marmleite@hotmail.com>
2020-07-18 14:37:42 -0300

    fix name test_serior_spec file fix name class RSpec

---
+++ spec/test_senior_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe SeniorTest do
+  context "with invalid argument" do
+    it 'returns invalid argument' do
+      expect(SeniorTest.evaluate_seniority).to eq('invalid argument')
+    end      
+  end
+end
