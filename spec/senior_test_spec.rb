3dbdc5afa6107ea4379aa20d266e8661965ea4ee
marcelo <marmleite@hotmail.com>
2020-07-18 15:29:50 -0300

    fix it definitions senior_test_spec

--- spec/senior_test_spec.rb
+++ spec/senior_test_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe SeniorTest do
     end      
   end
   context "with skill value above 5" do
-    it 'returns invalid argument' do
+    it "returns skill value can't be above 5" do
       person = [{ ruby: 7 }]
       expect(SeniorTest.validate_skills(person)).to eq("skill value can't be above 5")
     end      
   end
   context "with skill value under 0" do
-    it 'returns invalid argument' do
+    it "returns skill value can't be negative" do
       person = [{ ruby: -1 }]
       expect(SeniorTest.validate_skills(person)).to eq("skill value can't be negative")
     end      
