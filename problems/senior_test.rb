cd9cf18c560b9087039a43ccf6d2cccde7c0458e
marcelo <marmleite@hotmail.com>
2020-07-18 15:01:37 -0300

    test skill value above 5

--- problems/senior_test.rb
+++ problems/senior_test.rb
@@ -1,2 +1,9 @@
 class SeniorTest
+  class << self
+    def validate_skills(person = [])
+      if person.empty?
+        return 'invalid argument'
+      end
+    end
+  end
 end
--- spec/senior_test_spec.rb
+++ spec/senior_test_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe SeniorTest do
       expect(SeniorTest.validate_skills(person)).to eq('invalid argument')
     end      
   end
+  context "with skill value above 5" do
+    it 'returns invalid argument' do
+      person = [{ ruby: 7 }]
+      expect(SeniorTest.validate_skills(person)).to eq("skill value can't be above 5")
+    end      
+  end
 end
