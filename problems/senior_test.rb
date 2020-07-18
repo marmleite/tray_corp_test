945583ae2c5a8680882358305802c1bd3cca5cb9
marcelo <marmleite@hotmail.com>
2020-07-18 15:22:30 -0300

    validate skill value above 5

--- problems/senior_test.rb
+++ problems/senior_test.rb
@@ -1,9 +1,15 @@
 class SeniorTest
   class << self
     def validate_skills(person = [])
-      if person.empty?
-        return 'invalid argument'
+      error = 'invalid argument' if person.empty?
+      if person.any? { |skill_set| validate_skill_values(skill_set.values) }
+        error = "skill value can't be above 5"
       end
+      return error
+    end
+
+    def validate_skill_values(values)
+      values.any? { |value| value > 5 }
     end
   end
 end
