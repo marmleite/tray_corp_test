d8022de240d19e152ee0b1609a6f80d2668d7eba
marcelo <marmleite@hotmail.com>
2020-07-18 15:28:09 -0300

    validate skill negative

--- problems/senior_test.rb
+++ problems/senior_test.rb
@@ -2,14 +2,14 @@ class SeniorTest
   class << self
     def validate_skills(person = [])
       error = 'invalid argument' if person.empty?
-      if person.any? { |skill_set| validate_skill_values(skill_set.values) }
-        error = "skill value can't be above 5"
-      end
-      return error
+      person.each { |skill_set| error = validate_skill_values(skill_set.values) }
+      error
     end
-
+  
     def validate_skill_values(values)
-      values.any? { |value| value > 5 }
+      error = "skill value can't be above 5"  if values.any? { |value| value > 5 }
+      error = "skill value can't be negative"  if values.any? { |value| value < 0 }
+      error
     end
   end
 end
