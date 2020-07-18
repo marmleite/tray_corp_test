445923fcbe9cd2f77a3a56cbc5b503f66057a31a
marcelo <marmleite@hotmail.com>
2020-07-18 16:25:46 -0300

    test senior skills message

--- spec/senior_test_spec.rb
+++ spec/senior_test_spec.rb
@@ -7,18 +7,21 @@ RSpec.describe SeniorTest do
       expect(SeniorTest.validate_skills(person)).to eq('invalid argument')
     end      
   end
+  
   context "with skill value above 5" do
     it "returns skill value can't be above 5" do
       person = [{ ruby: 7 }]
       expect(SeniorTest.validate_skills(person)).to eq("skill value can't be above 5")
     end      
   end
+  
   context "with skill value under 0" do
     it "returns skill value can't be negative" do
       person = [{ ruby: -1 }]
       expect(SeniorTest.validate_skills(person)).to eq("skill value can't be negative")
     end      
   end
+  
   context "with skills valid values" do
     it "returns skills values sum" do
       person = [{ ruby: 1, db: 1, agile: 0, pattern: 0  }]
@@ -51,6 +54,14 @@ RSpec.describe SeniorTest do
       person = [{ ruby: 5, db: 3, agile: 1, pattern: 2 }]
       values_sum = person.first.values.sum
       expect(SeniorTest.evaluate_seniority(person)).to eq("Pleno – Pontuação (#{values_sum} pontos)")
+    end      
+  end
+
+  context "with skills values sum greather than 11" do
+    it "returns senior skills message" do
+      person = [{ ruby: 5, db: 4, agile: 3, pattern: 3, server: 2 }]
+      values_sum = person.first.values.sum
+      expect(SeniorTest.evaluate_seniority(person)).to eq("Sênior – Pontuação (#{values_sum} pontos)")
     end      
   end
 end
