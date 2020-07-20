663c2a5cb5e938bcfa842a9d3f4038b57f893511
marcelo <marmleite@hotmail.com>
2020-07-20 12:54:58 -0300

    change error messages to raise error SeniorTest

--- problems/senior_test.rb
+++ problems/senior_test.rb
@@ -1,22 +1,20 @@
 class SeniorTest
   class << self
     def evaluate_seniority(person = [])
-      error_message = validate_skills(person)
+      validate_skills(person)
       skills_sum = sum_skills_values(person)
       level = seniority_level(skills_sum)
       "#{level} – Pontuação (#{skills_sum} pontos)"
     end
 
     def validate_skills(person = [])
-      error = 'invalid argument' if person.empty?
+      raise 'invalid argument' if person.empty?
       person.each { |skill_set| error = validate_skill_values(skill_set.values) }
-      error
     end
   
     def validate_skill_values(values)
-      error = "skill value can't be above 5"  if values.any? { |value| value > 5 }
-      error = "skill value can't be negative"  if values.any? { |value| value < 0 }
-      error
+      raise "skill value can't be above 5"  if values.any? { |value| value > 5 }
+      raise "skill value can't be negative"  if values.any? { |value| value < 0 }
     end
 
     def sum_skills_values(person)
