342b2ca59b91770347e5e274dfb44f805939cd21
marcelo <marmleite@hotmail.com>
2020-07-20 13:56:37 -0300

    add exception blocks to runner

--- runner.rb
+++ runner.rb
@@ -7,10 +7,21 @@ problem_args = ARGV[1..-1].join(' ')
 
 case problem
 when 'senior_test'
-  parsed_args = eval(problem_args)
-  puts SeniorTest.evaluate_seniority(parsed_args)
+  begin
+    parsed_args = eval(problem_args)
+    puts SeniorTest.evaluate_seniority(parsed_args)
+  rescue => exception
+    puts exception.message    
+  end
 when 'vocals_test'
-  puts VocalsTest.sum(problem_args)
+  begin
+    puts VocalsTest.sum(problem_args)
+  rescue => exception
+    puts exception.message    
+  end
 else
-  'Invalid arguments'  
+  puts 'Invalid problem!'
+  puts 'Choose a valid problem: senior_test or vocals_test.'
+  puts "EX: $ ./run vocals_test 'message to count vocals'"
+  puts 'See README for more details.'
 end
